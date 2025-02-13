eb889f10b18370848bfc3fadf80a8fa836abaaf6
Mike Plachta <mplachta@users.noreply.github.com>
2025-02-13 14:28:39 -0800

    Groq demo changes.

--- src/conversational_routing/crews/assistant_crew/assistant_crew.py
+++ src/conversational_routing/crews/assistant_crew/assistant_crew.py
@@ -2,31 +2,7 @@ import os
 import glob
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-from crewai.knowledge.source.csv_knowledge_source import CSVKnowledgeSource
-from crewai.knowledge.source.text_file_knowledge_source import TextFileKnowledgeSource
-
-# Define base path to current file
-knowledge_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../knowledge"))
-
-knowledge_enterprise_kb = CSVKnowledgeSource(
-    file_paths=["enterprise_kb.csv"],
-    metadata={
-        "category": "Enterprise Platform",
-    },
-)
-
-print(os.path.join(knowledge_base_path, "oss-docs/**/*.mdx"))
-
-# Prepare the knowledge base for the OSS Framework
-files = glob.glob(os.path.join(knowledge_base_path, "oss-docs/**/*.mdx"), recursive=True)
-files = [os.path.relpath(file, knowledge_base_path) for file in files]
-
-oss_framework_kb = TextFileKnowledgeSource(
-    file_paths=files,
-    metadata={
-        "category": "CrewAI Open Source Framework",
-    },
-)
+from crewai_tools import ScrapeWebsiteTool, SerperDevTool
 
 @CrewBase
 class AssistantCrew:
@@ -39,7 +15,7 @@ class AssistantCrew:
     def crewai_expert_agent(self) -> Agent:
         return Agent(
             config=self.agents_config["crewai_expert_agent"],
-            knowledge_sources=[knowledge_enterprise_kb, oss_framework_kb],
+            tools=[SerperDevTool(), ScrapeWebsiteTool()],
         )
 
     @task
