d2731e57b152afc48b24a351c8b3a54b62d90e65
Mike Plachta <mike@crewai.com>
2025-04-28 11:50:49 -0700

    Improving local development

--- test_chat.py
+++ test_chat.py
@@ -1,12 +1,17 @@
 from src.conversational_routing.main import ChatFlow
 from colorama import Fore, Style
 
+messages = [
+    "What is an Agent?",
+    "How to set up Azure OpenAI connection?"
+]
+
 # First chat message
 print(f"{Fore.BLUE}{Style.BRIGHT}Question #1{Style.RESET_ALL}")
 
 chat_flow = ChatFlow()
 response = chat_flow.kickoff(inputs={
-    "current_message": "What is an Agent?",
+    "current_message": messages[0],
 })
 
 print(f"{Style.BRIGHT}Response:{Style.RESET_ALL} ", response)
@@ -18,7 +23,7 @@ print(f"\n{Fore.BLUE}{Style.BRIGHT}Question #2{Style.RESET_ALL}")
 
 chat_flow = ChatFlow()
 response = chat_flow.kickoff(inputs={
-    "current_message": "How to set up Azure OpenAI connection?",
+    "current_message": messages[1],
     "id": id
 })
 
