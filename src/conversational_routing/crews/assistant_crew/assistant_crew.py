cdf3309547d542da39df0600d826e7863a7a9ae0
Mike Plachta <mike@crewai.com>
2025-04-28 11:22:53 -0700

    Moving knowledge to crew class

--- src/conversational_routing/crews/assistant_crew/assistant_crew.py
+++ src/conversational_routing/crews/assistant_crew/assistant_crew.py
@@ -6,42 +6,41 @@ from crewai.knowledge.source.text_file_knowledge_source import TextFileKnowledge
 from crewai.knowledge.knowledge_config import KnowledgeConfig
 from pathlib import Path
 
-# Define base path to current file
-knowledge_base_path = Path(__file__).parent / "knowledge"
-
-# Prepare the knowledge base for the OSS Framework
-files = glob.glob(os.path.join(knowledge_base_path, "oss-docs/**/*.mdx"), recursive=True)
-# Convert file strings to Path objects
-file_paths = [Path(file) for file in files]
-
-print("file_paths", file_paths)
-
-oss_framework_kb = TextFileKnowledgeSource(
-    file_paths=file_paths,
-    metadata={
-        "category": "CrewAI",
-    },
-)
-
-llm = LLM(
-    model="gpt-4o",
-    temperature=0.1,
-)
-
 @CrewBase
 class AssistantCrew:
     """Assistant Crew"""
 
+    # Crew Files
     agents_config = "config/agents.yaml"
     tasks_config = "config/tasks.yaml"
 
+    # LLM Configuration
+    llm = LLM(
+        model="gpt-4o",
+        temperature=0.1,
+    )
+
     @agent
     def crewai_expert_agent(self) -> Agent:
+        # Knowledge Configuration
+        # Define base path to current file
+        knowledge_base_path = Path(__file__).parent / "knowledge"
+
+        # Prepare the knowledge base for the OSS Framework
+        files = glob.glob(os.path.join(knowledge_base_path, "oss-docs/**/*.mdx"), recursive=True)
+        # Convert file strings to Path objects
+        knowledge_file_paths = [Path(file) for file in files]
+
         return Agent(
             config=self.agents_config["crewai_expert_agent"],
-            knowledge_sources=[oss_framework_kb],
+            knowledge_sources=[TextFileKnowledgeSource(
+                file_paths=knowledge_file_paths,
+                metadata={
+                    "category": "CrewAI",
+                },
+            )],
             knowledge_config=KnowledgeConfig(results_limit=5, score_threshold=0.7),
-            llm=llm,
+            llm=self.llm,
         )
 
     @task
@@ -57,6 +56,6 @@ class AssistantCrew:
             agents=self.agents,  # Automatically created by the @agent decorator
             tasks=self.tasks,  # Automatically created by the @task decorator
             process=Process.sequential,
-            verbose=False,
+            verbose=True,
             memory=True,
         )
